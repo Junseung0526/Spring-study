3d46172431b2e863fdf5e1e8f8e9c326e08881b7
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2025-12-14 21:20:21 +0900

    Chapter9 CRUD&SQL 쿼리 종합

--- src/main/java/com/spring/study/entity/Article.java
+++ src/main/java/com/spring/study/entity/Article.java
@@ -3,6 +3,7 @@ package com.spring.study.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -15,7 +16,7 @@ import lombok.*;
 public class Article {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
---
+++ src/main/resources/templates/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(title, content) VALUES ('1111', '일일일일');
+INSERT INTO article(title, content) VALUES ('2222', '이이이이');
+INSERT INTO article(title, content) VALUES ('3333', '삼삼삼삼');
