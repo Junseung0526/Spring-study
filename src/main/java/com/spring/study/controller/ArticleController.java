68c112e412880080d1f4d86ab265a4dab69a6f2e
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2025-12-14 11:59:46 +0900

    Chapter4 CREATE 리팩터링 적용

--- src/main/java/com/spring/study/controller/ArticleController.java
+++ src/main/java/com/spring/study/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.spring.study.controller;
 import com.spring.study.dto.ArticleForm;
 import com.spring.study.entity.Article;
 import com.spring.study.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     //스프링 부트가 미리 생성해 놓은 레포지토리 객체 주임 (DI)
@@ -21,15 +23,14 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm articleForm) {
-        System.out.println(articleForm.toString());
-
         // 1. DTO를 엔티티로 변환
         Article article = articleForm.toEntity();
-        System.out.println(article.toString());
+        log.info(articleForm.toString());
 
-        // 2. Repository로 엔티티를 DB에 저장
+        // 2. 레포지토리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+
         return "";
     }
 }
